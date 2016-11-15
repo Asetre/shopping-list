89ecbe66b9a10939d00c18d4810fba16dac5a297
Asetr <asetre.paul@gmail.com>
2016-11-15 14:42:05 -0800

    second commit

--- app.js
+++ app.js
@@ -1,28 +1,38 @@
 var state = {
-	item: [];
+	item: []
 }
 
 var addItem = function(state, item) {
 	state.item.push(item);
 }
 
-var renderItems = function (state, element) {
-	var itemsHTML = state.item.map(function(item) {
-		return '<li>'+'<span class="shopping-item">'+ item + '<div class="shopping-item-controls">' + '<button class="shopping-item-toggle">'+ '<span class="button-label">'+ 'check' + '</span>' '</button>' + '<button class="shopping-item-delete">' + 'delete' + '<span class="button-label">' + 'delete' + '</span>' + '</button>' + '</div>' + '</span>' + '</li>'
+var renderItems = function (state) {
+	var itemsHTML = state.item.map(function(items) {
+		var x = '<li>' +
+        '<span class="shopping-item">' + items + '</span>' +
+        '<div class="shopping-item-controls">' +
+          '<button class="shopping-item-toggle">' +
+            '<span class="button-label">check</span>' +
+          '</button>' +
+          '<button class="shopping-item-delete">' +
+            '<span class="button-label">delete</span>' +
+          '</button>' +
+        '</div>' +
+      '</li>'
 	});
-	element.html(itemsHTML);
+	console.log("test")
 }
 
 
 
-
-
-$('js-shopping-list-form').submit(function (event) {
+$('#js-shopping-list-form').submit(function (event) {
 
 	event.preventDefault();
 
 	addItem(state, $('input').val());
-	renderItems(state, )	
+	renderItems(state);
+	console.log("working");
 
 
 });
+
